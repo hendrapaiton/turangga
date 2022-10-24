5464e4276f85706efcfd370822ef00ab0812dafd
Hendra Dwi Saputra <hendra@usa.com>
2022-10-25 00:23:08 +0700

    Print title from website to console

--- turangga/spiders/nama.py
+++ turangga/spiders/nama.py
@@ -4,7 +4,7 @@ import scrapy
 class NamaSpider(scrapy.Spider):
     name = 'nama'
     allowed_domains = ['id.theasianparent.com']
-    start_urls = ['http://id.theasianparent.com/']
+    start_urls = ['https://id.theasianparent.com/nama-bayi-terinspirasi-dari-binatang']
 
-    def parse(self, response):
-        pass
+    def parse(self, response, **kwargs):
+        print('Response: ', response.xpath('//strong').get())
