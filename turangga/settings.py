6394f147764d9bfce69358376fa4c46ee13bc8f3
Hendra Dwi Saputra <hendra@usa.com>
2022-10-25 08:34:01 +0700

    Change settings to concurrent=1 and USER_AGENT as well

--- turangga/settings.py
+++ turangga/settings.py
@@ -14,13 +14,14 @@ NEWSPIDER_MODULE = 'turangga.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'turangga (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36' \
+             '(KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
