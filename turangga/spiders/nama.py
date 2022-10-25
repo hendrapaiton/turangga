dc9362ec68f1a3c3d125ec6b4b39497aaa53f473
Hendra Dwi Saputra <hendra@usa.com>
2022-10-25 22:33:50 +0700

    Add regex to remove unicode symbols

--- turangga/spiders/nama.py
+++ turangga/spiders/nama.py
@@ -7,12 +7,14 @@ from turangga.items import TuranggaItem
 class NamaSpider(scrapy.Spider):
     name = 'nama'
     allowed_domains = ['id.theasianparent.com']
-    start_urls = ['https://id.theasianparent.com/nama-bayi-terinspirasi-dari-binatang']
+    start_urls = [
+        'https://id.theasianparent.com/nama-bayi-terinspirasi-dari-binatang']
 
     def parse(self, response, **kwargs):
         item = TuranggaItem()
-        item['title'] = [re.sub(r'\d+\.\s*', "", v).replace(u'\xa0', '') for v in
+        item['title'] = [re.sub(r'\d+\.\s*', '', v).replace(u'\xa0', '') for v in
                          response.xpath('//strong/text()').extract()
                          if v != "Artikel terkait:"][:-4]
-        item['detail'] = response.xpath('//p/text()')[4:-19].extract()
+        item['detail'] = [re.sub(u'(\u2018|\u2019)', ',', v).replace(u'\xa0', '')
+                          for v in response.xpath('//p/text()').extract()][4:-19]
         yield item
